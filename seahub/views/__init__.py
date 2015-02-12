bda5e321aff460f8cb0332ef4144b766ed3e2b0c
[s-long] <[slong@lesechos.fr]>
2015-02-12 15:33:47 +0100

    Supprimer la redirection pour l'utilisateur non staff

--- seahub/views/__init__.py
+++ seahub/views/__init__.py
@@ -1056,14 +1056,6 @@ def myhome(request):
     repo_create_url = reverse("repo_create")
 
 
-    #Rediriger l'utilisateur non staff sur son propre repo du groupe
-    if not request.user.is_staff:
-        from seahub.utils import get_user_repos
-        owned_repos, shared_repos, groups_repos, public_repos = get_user_repos(username)
-        for repo in groups_repos:
-            redirect_to='/repo/' + repo.props.id
-        return HttpResponseRedirect(redirect_to)
-
     return render_to_response('myhome.html', {
             "owned_repos": owned_repos,
             "create_shared_repo": False,
